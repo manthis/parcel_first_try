0a8de74acf4172c634b332a364b3b40647c45bf5
Maxime AUBURTIN <maxime.auburtin@dockermail.io>
2018-07-10 22:27:29 +0200

    Optimized

--- index.js
+++
@@ -1,6 +0,0 @@
-
-import { message } from './src/lib';
-import './src/styles.scss';
-
-document.write(message);
-console.log('Ok app is now running...');
---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+import 'babel-polyfill';
+
+import { message } from './app';
+import './styles.scss';
+
+document.write(message);
+console.log('Ok app is now running...');
